ec1735d94602f45d62e2cccf12fb3a2293c94c72
EdenArts <ojedaeden08@gmail.com>
2021-09-09 08:41:41 -0700

    cursor lock and a cube pyramid were added

--- UnityProject/Assets/Scripts/Player/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public Transform camera;
     public CharacterController controller;
     
-    public float speed = 6f;
+    public float playerMovementSpeed = 6f;
 
     public float turnSmoothTime = 0.1f;
 
@@ -23,12 +23,14 @@ public class PlayerMovement : MonoBehaviour
         //checks for movement on any direction
         if(direction.magnitude >= 0.1f)
         {
+            //Makes the character turn in the direction the camera is facing
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+            //Moves the character based on what position the camera is facing
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            controller.Move(moveDirection.normalized * playerMovementSpeed * Time.deltaTime);
         }
     }
 }
